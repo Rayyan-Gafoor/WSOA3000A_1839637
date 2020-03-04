86bf9ca8ab41c1874aea94ec4a87dd5d8ff9d0a6
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2020-03-04 09:57:57 +0200

    Update Java.js
    java button update

--- Java.js
+++ Java.js
@@ -1,5 +1,10 @@
 var button= document.createElement("button");
 button.innerHTML="Menu";
+
 var body= document.getElementsByTagName("body")[0];
 body.appendChild(button)
 
+button.addEventListener ("click", function()
+{
+    alert("did something");
+});
