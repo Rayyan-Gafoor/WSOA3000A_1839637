ccdc0eee4c0fdce6c9d1e6201d251be7ce8b0bd3
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2020-03-09 15:49:12 +0200

    button
    button update using java file

--- Java.js
+++ Java.js
@@ -1,11 +1,11 @@
-// 
-// var button= document.createElement("button");
-// button.innerHTML="Menu";
+ 
+ var button= document.createElement("button");
+button.innerHTML="Menu";
 
-// var body= document.getElementsByTagName("body")[0];
-// body.appendChild(button)
+var body= document.getElementsByTagName("body")[0];
+body.appendChild(button)
 
-// button.addEventListener ("click", function()
-// {
-//     alert("did something");
-// });
+button.addEventListener ("click", function()
+{
+     alert("did something");
+});
