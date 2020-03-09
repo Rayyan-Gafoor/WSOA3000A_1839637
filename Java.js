953d550c0d34f34785fd885457656151287a8a4e
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2020-03-09 16:58:07 +0200

    java button
    button

--- Java.js
+++ Java.js
@@ -1,11 +1,14 @@
- 
- var button= document.createElement("button");
-button.innerHTML="Menu";
+ const button= document.body.children[0];
+ console.log(button);
 
-var body= document.getElementsByTagName("body")[0];
-body.appendChild(button)
-
-button.addEventListener ("click", function()
+ function random(number)
+ {
+     return Math.floor(Math.random()* (number+1));
+ }
+button.addEventListener ("click", function(e)
 {
-     alert("did something");
+    const colour= 'rgb('+ random(255) + ','+ random(255)+','+ random(255) +')';
+    document.body.style.backgroundColor= colour;
+     //alert("change colour");
+     console.log("pressed");
 });
