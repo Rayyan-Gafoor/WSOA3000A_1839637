ab8d1e9c17ec0a262f860560e803556a6856c33a
Rayyan-Gafoor <60914125+Rayyan-Gafoor@users.noreply.github.com>
2020-06-26 19:32:43 +0200

    javascript driven nav added

--- Java.js
+++ Java.js
@@ -13,6 +13,34 @@ button.addEventListener ("click", function(e)
      console.log("pressed");
 });
 
+var timeout	= 0;
+var closetimer	= 1000;
+var item= 0;
+
+function drop(id){	
+
+    if(closetimer){
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+	if(item) {
+        item.style.visibility = 'hidden';
+    }
+	item = document.getElementById(id);
+	item.style.visibility = 'visible';
+
+}
+function dropclose(){
+    if(item) {
+        item.style.visibility = 'hidden';
+    }   
+}
+function close(){
+    closetimer = window.setTimeout(dropclose, timeout);
+}
+document.ondblclick = dropclose;
+document.onclick=drop;
+
 
 
 
--- resources/war/War.js
+++ resources/war/War.js
@@ -1,42 +1,28 @@
 var timeout	= 0;
-var closetimer	= 0;
+var closetimer	= 1000;
 var item= 0;
 
-// open hidden layer
-function drop(id)
-{	
-	// cancel close timer
-	mcancelclosetime();
+function drop(id){	
 
-	// close old layer
-	if(item) item.style.visibility = 'hidden';
-
-	// get new layer and show it
+    if(closetimer){
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+	if(item) {
+        item.style.visibility = 'hidden';
+    }
 	item = document.getElementById(id);
 	item.style.visibility = 'visible';
 
 }
-// close showed layer
-function mclose()
-{
-	if(item) item.style.visibility = 'hidden';
+function dropclose(){
+    if(item) {
+        item.style.visibility = 'hidden';
+    }   
 }
-
-// go close timer
-function close()
-{
-	closetimer = window.setTimeout(mclose, timeout);
+function close(){
+    closetimer = window.setTimeout(dropclose, timeout);
 }
+document.ondblclick = dropclose;
+document.onclick=drop;
 
-// cancel close timer
-function mcancelclosetime()
-{
-	if(closetimer)
-	{
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
-}
-
-// close layer when click-out
-document.onclick = mclose; 
